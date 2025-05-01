310ede5066f374071b246ec3fdbb79ea1db91fea
umerbinzarrar <164541713+umerbinzarrar@users.noreply.github.com>
2025-05-02 04:08:10 +0500

    Added event listeners to the inputs and stored user input in a state variable

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import './ExpenseForm.css'
+import React, {useState} from 'react';
+import './ExpenseForm.css';
+
 function ExpenseForm() {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const titleChangeHandeler = (event)=>{
+        setEnteredTitle(event.target.value)
+    }
+    const amountChangeHandeler = (event)=>{
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandeler = (event)=>{
+        setEnteredDate(event.target.value)
+    }
+
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text"/>
+                    <input type="text" onChange = {titleChangeHandeler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min='0.01' step='0.01'/>
+                    <input type="number" min='0.01' step='0.01' onChange={amountChangeHandeler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min = '2024-01-01' max='2025-12-31'/>
+                    <input type="date" min = '2024-01-01' max='2025-12-31' onChange={dateChangeHandeler}/>
                 </div>
                 <div className="new-expense__actions">
                     <button type="submit">Add Expense</button>
