368005f8cdf5efb9c5932cb2c86c5649b7e0ea8e
umerbinzarrar <164541713+umerbinzarrar@users.noreply.github.com>
2025-05-05 23:46:00 +0500

    Added filter component not sure if it is working properly

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import NewExpense from "./components/NewExpense";
 
 
 const dumm_expenses = [
-  {id: 'u1', title:"Burger", amount:150 , date: new Date(2025,3,26)},
+  {id: 'u1', title:"Burger", amount:150 , date: new Date(2024,3,26)},
   {id: 'u2', title:"Shave", amount:25 , date: new Date(2025,1,15)},
   {id: 'u3', title:"Fuel", amount:250 , date: new Date(2025,5,24)},
-  {id: 'u4', title:"Car Insurance", amount:250 , date: new Date(2025,1,5)},
+  {id: 'u4', title:"Car Insurance", amount:250 , date: new Date(2022,1,5)},
 ];
 function App() {
 
@@ -17,9 +17,7 @@ const addExpenseHandler = expense =>{
   setExpenses((prevExpenses) => {
     return [expense, ...prevExpenses];
   });
-  // expenses.push(expense);
-  // console.log('in App.js');
-  // console.log(expense);
+
 };
 
   return (
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,36 @@
+import React, {useState}  from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "./Card";
 
 function Expenses(props) {
+
+    const [filteredYear, setFilteredYear] = useState('2023');
+    const filterChangeHandler = selectedYear =>{
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.expenses.filter(expense =>{
+        return expense.date.getFullYear().toString() === filteredYear; 
+    })
+
+
+
     return (
-        <div className="expenses">
+
+        <Card className="expenses">
+
+            <ExpensesFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler} />
+
+            {filteredExpenses.map((expense) =>{
+
+             return  <ExpenseItem key = {expense.id} title={expense.title} amount={expense.amount} date={expense.date}  />
+            })}            
+
             
 
-            {props.expenses.map(expense=> <ExpenseItem key = {expense.id} title={expense.title} amount={expense.amount} date={expense.date}  />) }
-
            
-        </div>
+        </Card>
     );
 
 }
